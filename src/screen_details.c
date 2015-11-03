71db480cf21c20fa79e9a48fc6ef744063f2a40e
David Stosik <david.stosik+git-noreply@gmail.com>
2015-11-03 18:35:25 +0900

    Fix timer for repeating up click.

--- src/screen_details.c
+++ src/screen_details.c
@@ -86,7 +86,7 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_repeating_click_subscribe(BUTTON_ID_UP, 40, up_click_handler);
+  window_single_repeating_click_subscribe(BUTTON_ID_UP, 100, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
   window_single_repeating_click_subscribe(BUTTON_ID_DOWN, 100, down_click_handler);
 
